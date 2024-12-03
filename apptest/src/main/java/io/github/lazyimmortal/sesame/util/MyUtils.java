0ba7763cd92a8c5837cc532ee61df5fa0f04ce17
khaos <khaos@qq.com>
2024-12-03 11:21:55 +0700

    MyUtils

--- apptest/src/main/java/io/github/lazyimmortal/sesame/util/MyUtils.java
+++ apptest/src/main/java/io/github/lazyimmortal/sesame/util/MyUtils.java
@@ -25,10 +25,10 @@ public class MyUtils {
   //打印用户切换
   public static void recordUserName(@Nullable String uid) {
     if (TextUtils.isEmpty(uid)) return;
-    UserEntity user = UserIdMap.get(uid);
-    if (user != null) {
-      Log.record("加载用户:" + user.getNickName());
-    }
+    //UserEntity user = UserIdMap.get(uid);
+    //if (user != null) {
+    //  Log.record("加载用户:" + user.getNickName());
+    //}
   }
 
   //首页显示全部记录，菜单显示其他记录
