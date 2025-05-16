c3ce9f778b6179674a723fc9a6237e878ec51c8b
khaos <khaos@qq.com>
2025-05-16 16:34:59 +0700

    增加能量雨参数senlinguangchuangrukou

--- app/src/main/java/io/github/lazyimmortal/sesame/model/task/antForest/AntForestRpcCall.java
+++ app/src/main/java/io/github/lazyimmortal/sesame/model/task/antForest/AntForestRpcCall.java
@@ -116,7 +116,7 @@ public class AntForestRpcCall {
     }
 
     public static String queryEnergyRainHome() {
-        return ApplicationHook.requestString("alipay.antforest.forest.h5.queryEnergyRainHome", "[{\"version\":\"" + VERSION + "\"}]");
+        return ApplicationHook.requestString("alipay.antforest.forest.h5.queryEnergyRainHome", "[{\"source\":\"senlinguangchuangrukou\",\"version\":\""+VERSION+"\"}]");
     }
 
     public static String queryEnergyRainCanGrantList() {
