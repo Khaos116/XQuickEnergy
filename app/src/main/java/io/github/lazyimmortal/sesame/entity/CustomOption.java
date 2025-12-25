8ed347fc09266cc0874eb1d796c2cdd975b66179
khaos116 <khaos116@qq.com>
2025-12-25 09:33:26 +0700

    开启选项

--- app/src/main/java/io/github/lazyimmortal/sesame/entity/CustomOption.java
+++ app/src/main/java/io/github/lazyimmortal/sesame/entity/CustomOption.java
@@ -41,7 +41,7 @@ public class CustomOption extends IdAndName {
         list.add(new CustomOption("familyFeed", "帮喂成员"));
         list.add(new CustomOption("familyEatTogether", "美食请客"));
         list.add(new CustomOption("familyClaimReward", "领取奖励"));
-        //list.add(new CustomOption("deliverMsgSend", "道早安"));
+        list.add(new CustomOption("deliverMsgSend", "道早安"));
         list.add(new CustomOption("assignRights", "顶梁柱"));
         return list;
     }
