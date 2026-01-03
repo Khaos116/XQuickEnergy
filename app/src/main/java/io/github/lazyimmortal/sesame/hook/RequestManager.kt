0e2cf02d7865563caf7a4a34c899d82a3d80b355
khaos116 <khaos116@qq.com>
2026-01-04 00:10:55 +0700

    绿色经营同步TK版本

--- app/src/main/java/io/github/lazyimmortal/sesame/hook/RequestManager.kt
+++ app/src/main/java/io/github/lazyimmortal/sesame/hook/RequestManager.kt
@@ -5,7 +5,7 @@ package io.github.lazyimmortal.sesame.hook
  * Date:2026/1/3
  * Time:23:56
  */
-class RequestManager {
+object RequestManager {
   @JvmStatic
   fun requestString(method: String?, data: String?): String {
     return ApplicationHook.requestString(method ?: "", data ?: "") ?: "{}"
