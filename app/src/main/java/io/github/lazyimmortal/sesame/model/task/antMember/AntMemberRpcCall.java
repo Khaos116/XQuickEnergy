5a2822a70e01f3c314c7a54ed4081d4d73eb6e71
khaos116 <khaos116@qq.com>
2026-01-19 11:06:29 +0700

    同步：玩乐豆小球请求方法

--- app/src/main/java/io/github/lazyimmortal/sesame/model/task/antMember/AntMemberRpcCall.java
+++ app/src/main/java/io/github/lazyimmortal/sesame/model/task/antMember/AntMemberRpcCall.java
@@ -169,7 +169,7 @@ public class AntMemberRpcCall {
      * 游戏中心签到
      */
     public static String continueSignIn() {
-        return ApplicationHook.requestString("com.alipay.gamecenteruprod.biz.rpc.continueSignIn", "[{\"sceneId\":\"GAME_CENTER\",\"signType\":\"NORMAL_SIGN\",\"source\":\"ch_appcenter__chsub_9patch\"}]");
+        return ApplicationHook.requestString("com.alipay.gamecenteruprod.biz.rpc.continueSignIn", "[{\"sceneId\":\"GAME_CENTER\",\"signType\":\"NORMAL_SIGN\"}]");
     }
 
     /**
@@ -184,6 +184,22 @@ public class AntMemberRpcCall {
      */
     public static String batchReceivePointBall() {
         return ApplicationHook.requestString("com.alipay.gamecenteruprod.biz.rpc.v3.batchReceivePointBall", "[{}]");
+    }
+
+    public static String doTaskSignup(String taskId) {
+        return ApplicationHook.requestString("com.alipay.gamecenteruprod.biz.rpc.v3.doTaskSignup", "[{\"taskId\":\"" + taskId + "\"}]");
+    }
+
+    public static String doTaskSend(String taskId) {
+        return ApplicationHook.requestString("com.alipay.gamecenteruprod.biz.rpc.v3.doTaskSend", "[{\"taskId\":\"" + taskId + "\"}]");
+    }
+
+    public static String queryModularTaskList() {
+        return ApplicationHook.requestString("com.alipay.gamecenteruprod.biz.rpc.v3.queryModularTaskList", "[{\"deviceLevel\":\"high\",\"source\":\"ch_appcollect__chsub_my-recentlyUsed\",\"sourceTab\":\"luckydraw\",\"unityDeviceLevel\":\"high\"}]");
+    }
+
+    public static String queryTaskList() {
+        return ApplicationHook.requestString("com.alipay.gamecenteruprod.biz.rpc.v4.queryTaskList", "[{\"__git\":\"52f2c9969ae\",\"source\":\"ch_alipaysearch__chsub_normal\"}]");
     }
 
     /**
