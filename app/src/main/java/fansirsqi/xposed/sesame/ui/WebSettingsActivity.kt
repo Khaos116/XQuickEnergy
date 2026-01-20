e156a285aac5e53d533fb5f04845b73e98622edf
khaos116 <khaos116@qq.com>
2026-01-20 15:59:34 +0700

    改为kt

--- app/src/main/java/fansirsqi/xposed/sesame/ui/WebSettingsActivity.kt
+++ app/src/main/java/fansirsqi/xposed/sesame/ui/WebSettingsActivity.kt
@@ -1,16 +1,17 @@
-package fansirsqi.xposed.sesame.ui;
+package fansirsqi.xposed.sesame.ui
 
-import android.content.Intent;
-import android.os.Bundle;
+import android.content.Intent
+import android.os.Bundle
+import fansirsqi.xposed.sesame.util.MyUtils
 
-public class WebSettingsActivity extends BaseActivity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    Intent intent = new Intent(this, WebSettingsActivity.class);
-    intent.putExtra("userId", intent.getStringExtra("userId"));
-    intent.putExtra("userName", intent.getStringExtra("userName"));
-    finish();
-    startActivity(intent);
+class WebSettingsActivity : BaseActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    MyUtils.CHANGE_KT5.trim()
+    val intent = Intent(this, WebSettingsActivity::class.java)
+    intent.putExtra("userId", intent.getStringExtra("userId"))
+    intent.putExtra("userName", intent.getStringExtra("userName"))
+    finish()
+    startActivity(intent)
   }
 }
