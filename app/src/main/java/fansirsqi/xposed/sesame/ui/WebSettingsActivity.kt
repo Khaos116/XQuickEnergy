ffac89dccfb9680bdfcbee5d293b431895581389
khaos116 <khaos116@qq.com>
2026-01-21 18:06:50 +0700

    修复参数传递丢失的bug

--- app/src/main/java/fansirsqi/xposed/sesame/ui/WebSettingsActivity.kt
+++ app/src/main/java/fansirsqi/xposed/sesame/ui/WebSettingsActivity.kt
@@ -3,15 +3,23 @@ package fansirsqi.xposed.sesame.ui
 import android.content.Intent
 import android.os.Bundle
 import fansirsqi.xposed.sesame.util.MyUtils
+import fansirsqi.xposed.sesame.util.ToastUtil
 
 class WebSettingsActivity : BaseActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     MyUtils.CHANGE_KT5.trim()
-    val intent = Intent(this, SettingActivity::class.java)
-    intent.putExtra("userId", intent.getStringExtra("userId"))
-    intent.putExtra("userName", intent.getStringExtra("userName"))
+    val userId = intent?.getStringExtra("userId")
+    val userName = intent?.getStringExtra("userName")
+    if (userId.isNullOrBlank()) {
+      ToastUtil.showToast("用户ID不能为空")
+      finish()
+      return
+    }
+    val intentNew = Intent(this, SettingActivity::class.java)
+    intentNew.putExtra("userId", userId)
+    intentNew.putExtra("userName", userName)
     finish()
-    startActivity(intent)
+    startActivity(intentNew)
   }
 }
